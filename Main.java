7638fbe5800143c60a0b1c9a1fc5acfa550722b4
João Miguel <j.meier@edu.pucrs.br>
2025-05-01 01:04:06 +0000

    exercicio 1

---
+++ Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static ArrayList<Integer> calcularTroco(List<Integer> moedas, int valor) {
+        
+        ArrayList<Integer> resultado = new ArrayList<>();
+        int cont = 0;
+        
+        for (int moeda : moedas) {
+            while (valor >= moeda) {
+                valor -= moeda;
+                resultado.add(moeda);
+                cont++;
+            }
+        }
+        
+        System.out.println("Cont --> " + cont);
+        return resultado;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> moedas = new ArrayList<>();
+        moedas.add(100);
+        moedas.add(25);
+        moedas.add(10);
+        moedas.add(5);
+        moedas.add(1);
+        
+        // Cont --> 10
+        // Menor quantidade de moedas --> 10
+        // Moedas utilizadas --> [100, 100, 25, 25, 25, 10, 1, 1, 1, 1]
+        int valor = 289;
+        
+        ArrayList<Integer> troco = calcularTroco(moedas, valor);
+        System.out.println("Menor quantidade de moedas --> " + troco.size());
+        System.out.println("Moedas utilizadas --> " + troco);
+    }
+}
