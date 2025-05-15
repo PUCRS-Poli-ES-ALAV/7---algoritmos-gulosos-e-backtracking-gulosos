912a18daabc7d12ee945a02c3d02287904b1a936
Matheus Bueno de Oliveira <135169698+MatheusBuenodeOliveira@users.noreply.github.com>
2025-05-15 00:39:29 +0000

    nRainhas implementation

---
+++ backtrack/nRainhas.java
@@ -0,0 +1,59 @@
+package backtrack;
+public class nRainhas {
+
+    public static int[][] NRainhas(int NRainhas){
+        calculaNRainhas(tabuleiro,0, NRainhas );
+        return tabuleiro;         
+    }
+
+    public static int tabuleiro[][] = { { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0 },
+                 };
+
+    // function to check whether the position is safe or not 
+    static boolean ehSeguro(int [][]board, int row, int col)
+    {
+        int i, j;
+        for (i = 0; i < col; i++)
+            if (board[row][i] == 1)
+                return false;
+
+       
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+            
+        for (i = row, j = col; j >= 0 && i < row; i++, j--)
+            if (board[i][j] == 1)
+                return false;
+
+        return true;
+    }
+
+    // The function that solves the problem using backtracking 
+    public static boolean calculaNRainhas(int board[][], int col, int NRainhas)
+    {
+        if (col >= NRainhas)
+            return true;
+
+        for (int i = 0; i < NRainhas; i++) {
+            //if it is safe to place the queen at position i,col -> place it
+            if (ehSeguro(board, i, col)) {
+                board[i][col] = 1;
+
+                if (calculaNRainhas(board, col +1 , NRainhas))
+                    return true;
+
+                //backtrack if the above condition is false
+                board[i][col] = 0;
+            }
+        }
+        return false;
+    }
+
+}
